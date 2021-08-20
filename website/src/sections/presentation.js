1ce47ad6e89ffad349cf862bf21e2e820d35a48c
DaielChom <danielcarvajalpatino@gmail.com>
2021-08-20 09:53:11 -0500

    :pencil: change background color

--- website/src/sections/presentation.js
+++ website/src/sections/presentation.js
@@ -7,11 +7,14 @@ class Presentation extends Component {
             <div className='row section-border' id='home'>
                 <div className='presentation-label'>
                     <p className='center-align'>
+                        
+                        <br/>
                         <span className="govt-grey bold font-size-8 klavika-font" > GOVT </span>
                         <br />
                         <span className="white-text opificio-font font-size-4 analytics-span"> analytics </span>
+
                     </p>
-                    <p className="center-align govt-grey font-size-2 montserrat-font no-margin">Análisis y servicios inteligentes <br/> para el sector público.</p>
+                    <p className="center-align white-text font-size-2 montserrat-font no-margin">Análisis y servicios inteligentes <br/> para el sector público.</p>
                 </div>
             </div>
         )
